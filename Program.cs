2963650d92bc93cfbc08dd7a5eec0f562e1d5f7b
DartHeise <rekun14336@gmail.com>
2022-02-23 21:04:29 +1000

    Assignment

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json;
+using System.Net;
+
+namespace TheWhiteSoftAssignment
+{
+    internal class Program
+    {
+        /* ИНСТРУКЦИЯ ДЛЯ ЗАПУСКА
+         * В переменную jsonReplacement передается путь к файлу replacement.json
+         * В переменную responce передается указанный URL
+         * Закомментирована возможность передать в responce путь к файлу data.json
+         * Программа запускается и создает файл result.json в указанном пути
+         */
+        static void Main()
+        {
+            var jsonReplacement = File.ReadAllText("replacement.json");
+            var replaceList = JsonSerializer.Deserialize<List<SourceReplacement>>(jsonReplacement);
+            var replacementDictionary = new Dictionary<string, string>();
+
+            replaceList.Sort(); // Сортируем список 
+            foreach (SourceReplacement el in replaceList)
+            {
+                if (!replacementDictionary.ContainsKey(el.replacement)) // Если ключ отсутствует в словаре:
+                {
+                    replacementDictionary.Add(el.replacement, el.source); // добавляем ключ и его значение
+                }
+                else
+                {
+                    replacementDictionary[el.replacement] = el.source; // иначе заменяем значение ключа
+                }
+            }
+
+            var webClient = new WebClient();
+           // var responce = File.ReadAllText("data.json");
+            var responce = webClient.DownloadString
+            ("https://raw.githubusercontent.com/thewhitesoft/student-2022-assignment/main/data.json");
+            var jsonData = JsonSerializer.Deserialize<string[]>(responce);
+            var result = new List<string>();
+
+            for (int i = 0; i < jsonData.Length; i++)
+            {
+                foreach (var values in replacementDictionary)
+                {
+                    jsonData[i] = jsonData[i].Replace(values.Key, values.Value); // Исправляем в строке измененные сообщения
+                }
+                if (!string.IsNullOrEmpty(jsonData[i]))
+                    result.Add(jsonData[i]); // Добавляем исправленную строку в список
+            }
+
+            var jsonResult = JsonSerializer.Serialize
+            (result.ToArray(), new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText("result.json", jsonResult); // Записываем результат в result.json
+        }
+    }
+}
---
+++ SourceReplacement.cs
@@ -0,0 +1,24 @@
+﻿namespace TheWhiteSoftAssignment
+{
+    internal class SourceReplacement: IComparable
+    {
+        public string replacement { get; set; }
+        public string source { get; set; }
+        public SourceReplacement(string replacement, string source)
+        {
+            this.replacement = replacement;
+            this.source = source;
+        }
+
+        public int CompareTo(object? obj) // Сортировка по длине ключа
+        {
+            SourceReplacement anotherReplacement = (SourceReplacement)obj;
+            if (replacement.Length > anotherReplacement.replacement.Length)
+                return -1;
+            else if (replacement.Length < anotherReplacement.replacement.Length)
+                return 1;
+
+            return 0;
+        }
+    }
+}
